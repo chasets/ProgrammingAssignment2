e665777b4bc9a5f407dcb469746ba79ad2d86a95
Tim Chase <tchase_lt@tsc2.local>
2014-05-18 10:21:00 -0400

    Example code changed to meet requirements for inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## TSC 2014-05-18
+## create a matrix type object that can get and set 
+## its own inverse (using the solve function)
+## If the inverse has alread been computed, then getsolve() will return 
+## the inverse; if not, use setsolve() to compute the inverse and cache
+## it inside the object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  
 }
 
 
-## Write a short comment describing this function
+## TSC 2014-05-18
+## use makeCacheMatrix to get the (possibly cached) inverse
+## of a matrix. 
+## If the inverse has alread been computed, then getsolve() will return 
+## the inverse; if not, use setsolve() to compute the inverse and cache
+## it inside the object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    s <- x$gesolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
