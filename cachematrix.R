063fde8d5dcdde796f421fb240ba7340a3311812
Tim Chase <tchase_lt@tsc2.local>
2014-05-24 10:35:47 -0400

    Added some testing.

--- cachematrix.R
+++ cachematrix.R
@@ -8,6 +8,23 @@
 ## the inverse; if not, use setsolve() to compute the inverse and cache
 ## it inside the object
 
+## TSC 2014-05-24
+## Adding some testing
+## Set up the matrices using the magic package
+# m3 <- makeCacheMatrix(magic(3))
+# m5 <- makeCacheMatrix(magic(5))
+# m2001 <- makeCacheMatrix(magic(2001))
+## Solve and check for the cache hit
+# cacheSolve(m3)
+# cacheSolve(m3)
+## Timing on a small matrix
+# system.time(cacheSolve(m5))
+# system.time(cacheSolve(m5))
+## Timing on a bigger matrix. 
+# system.time(cacheSolve(m2001))
+# system.time(cacheSolve(m2001))
+## 20 seconds on first; 0 seconds on the second
+
 makeCacheMatrix <- function(x = matrix()) {
     s <- NULL
     set <- function(y) {
@@ -33,7 +50,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    s <- x$gesolve()
+    s <- x$getsolve()
     if(!is.null(s)) {
         message("getting cached data")
         return(s)
